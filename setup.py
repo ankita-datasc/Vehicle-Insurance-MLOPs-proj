0959331c991c63c7dc06e1e43173ab52e23aaf23
ankita-datasc <ankitachandrakar2301@gmail.com>
2025-06-19 13:13:46 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ankita Chandrakar",
+    author_email="ankitachandrakar2301@gmail.com",
+    packages=find_packages()
+)
